78505873a93bbae5570ab520443554bb6357d6a7
yonathan reynaldo hernandez santos <hernandez.yonathan@uabc.edu.mx>
2023-02-19 19:10:00 -0800

    commit intento 3

---
+++ Clase.java
@@ -0,0 +1,6 @@
+public class Clase {
+  public static void main(String[] args) {
+	String name="Yonathan Hernandez";
+    System.out.println("Hola mundo, este repositorio fue creado por:"+name);
+  }
+}
